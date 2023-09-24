a4da24d949aa8ff6835b3df71f4e0ddc2310cbb3
h-akari <hieda.akari0930@gmail.com>
2023-09-24 21:51:51 +0900

    updated

--- background.js
+++ background.js
@@ -1,24 +1,36 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
- 
-  console.log(request)
-  sendResponse("i got data")
-  
-})
+/*chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
+ console.log(request)
+  var data = request
 
-fetch('http://localhost:3000/api/blinji/htmlSendingReq',{
+  console.log("3:receiving html popup=>background")
+
+  
+  fetch('http://localhost:3000/api/blinji/htmlSendingReq',{
   method:"POST",
   body: data
-})
+  })
   .then((response) => {
     
-    return response.json()
+    return  response.json()
   })
-  .then((data) => {
-   
-    console.log(data)
+  .then((result) => {
+    result = JSON.stringify(result)
+    console.log(result)//object value
+    console.log("5:receiving data api=>extension (background)" )
+    sendResponse(result)
+    
+  
+
 
   })
   .catch((error) => {console.log(error)})
+
+  return true;
+
+  
+})
+
+
 
 
 
@@ -63,3 +75,297 @@ chrome.tabs.query({active:true,currentWindow:true},function(tabs){ //select tab,
 }
 
 */
+
+
+//写し
+
+
+// main()
+
+
+//     async function main()
+// {
+ 
+      
+      
+// 	//アクティブなタブを取得
+// 	  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+// 	//アクティブなタブでJavaScript(parseDOM)を実行
+// 	        await chrome.scripting.executeScript({
+// 		    target:{tabId:tab.id, allFrames: true},
+// 		    func:parseDOM
+            
+//         }).then(injectionResults => {
+//             injectionResults = injectionResults[0].result
+
+//             data = injectionResults.toString()
+//             console.log(data)
+//             console.log("2:getting html success")
+           
+//             var newHomonymArray = getHardWord(data)
+                
+//                 console.log(newHomonymArray)
+                           
+          
+//                 //apiへ送り、easywordsを得る
+//                 for(var i=0; i<newHomonymArray.length; ++i){ 
+//                  fetch('http://localhost:3000/api/blinji/easyWordReq/' + newHomonymArray[i],{
+//                   method:"POST",
+//                   body: newHomonymArray[i]
+//                   })
+//                   .then((response) => {
+                    
+//                     return response.json()
+//                   })
+//                   .then((message2) => {
+//                     message2 = JSON.stringify(message2)
+//                     console.log(message2)//object value
+//                     console.log("5:receiving data api=>extension (background)" )
+
+//                     document.getElementById('result').innerHTML = message2
+                                        
+//                   })
+                  
+//                 }
+              
+// })
+// }
+
+
+
+
+
+
+          
+        
+        
+          
+
+//     function parseDOM()
+// {   
+//     console.log("1")
+//     return document.body.innerHTML;
+   
+    
+
+//     // chrome.runtime.sendMessage(textString, function(sendResponse){
+//     //     console.log(sendResponse)
+//     //     console.log("6:receiving data background=>popup")
+//     //     // sendResponse = JSON.stringify(sendResponse)
+//     //     // sendResponse = JSON.parse(sendResponse)
+    
+        
+//     // })
+
+// }   
+
+// function getHardWord(data)
+
+// {
+//   fetch('http://localhost:3000/api/blinji/gettingListReq')                
+//                 .then((response) => {
+                  
+//                   return  response.json()
+//                 })
+//                 .then((message1) => {
+//                   message1 = JSON.stringify(message1)
+//                   console.log(message1)
+//                   console.log("3:receiving data api=>extension " )
+//                   //hardwordリスト（文字列）を整える["hard","暑い","氏"]
+//                   var message1 = message1.replace(/[\"]/g,"") //""を消す
+//                   var message1 = message1.slice( 1 ) ;
+//                   var message1 = message1.slice( 0, -1 ) ;
+
+//                   console.log(message1)//hard,暑い,氏
+
+//                    var homonymArray = []
+//                    homonymArray = message1.split(",");
+//                    console.log(homonymArray)
+
+
+//                  // リストと一致するhardwordを調べる 
+//                  var newHomonymArray = []
+//                    for( var i =0; i< homonymArray.length; ++i ) {
+//                   if( data.match(homonymArray[i])){
+//                    var result = data.match(homonymArray[i]) 
+//                    console.log(result[0]) 
+//                    newHomonymArray.push(result[0])
+//                    }
+//                   }
+//                   console.log(newHomonymArray)
+//                   return newHomonymArray
+//                 }) 
+
+// }
+    
+
+// /*
+
+// main();
+// async function main()
+// {
+// 	//アクティブなタブを取得
+// 	let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+// 	//アクティブなタブでJavaScript(parseDOM)を実行
+// 	chrome.scripting.executeScript({
+// 		target:{tabId:tab.id},
+// 		func:parseDOM
+// 	}).then(function (r) {
+// 		//実行結果をポップアップウィンドウへ表示
+//         console.log(r)
+//         console.log(2)
+// 		document.getElementById('result').innerHTML = r[0].result;
+// 	});
+// }
+// function parseDOM()
+// {
+//     console.log('1')
+// 	return document.body.innerHTML;
+// }
+
+// */
+
+
+//原作
+
+
+// main()
+
+
+//     async function main()
+// {
+ 
+      
+      
+// 	//アクティブなタブを取得
+// 	  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+// 	//アクティブなタブでJavaScript(parseDOM)を実行
+// 	        await chrome.scripting.executeScript({
+// 		    target:{tabId:tab.id, allFrames: true},
+// 		    func:parseDOM
+            
+//         }).then(injectionResults => {
+//             injectionResults = injectionResults[0].result
+
+//             data = injectionResults.toString()
+//             console.log(data)
+//             console.log("2:getting html success")
+           
+//             fetch('http://localhost:3000/api/blinji/gettingListReq')                
+//                 .then((response) => {
+                  
+//                   return  response.json()
+//                 })
+//                 .then((message1) => {
+//                   message1 = JSON.stringify(message1)
+//                   console.log(message1)
+//                   console.log("3:receiving data api=>extension " )
+//                   //hardwordリスト（文字列）を整える["hard","暑い","氏"]
+//                   var message1 = message1.replace(/[\"]/g,"") //""を消す
+//                   var message1 = message1.slice( 1 ) ;
+//                   var message1 = message1.slice( 0, -1 ) ;
+
+//                   console.log(message1)//hard,暑い,氏
+
+//                    var homonymArray = []
+//                    homonymArray = message1.split(",");
+//                    console.log(homonymArray)
+
+
+//                  // リストと一致するhardwordを調べる 
+//                  var newHomonymArray = []
+//                    for( var i =0; i< homonymArray.length; ++i ) {
+//                   if( data.match(homonymArray[i])){
+//                    var result = data.match(homonymArray[i]) 
+//                    console.log(result[0]) 
+//                    newHomonymArray.push(result[0])
+//                    }
+//                   }
+//                   console.log(newHomonymArray)
+
+//                 //apiへ送り、easywordsを得る
+//                 for(var i=0; i<newHomonymArray.length; ++i){ 
+//                  fetch('http://localhost:3000/api/blinji/easyWordReq/' + newHomonymArray[i],{
+//                   method:"POST",
+//                   body: newHomonymArray[i]
+//                   })
+//                   .then((response) => {
+                    
+//                     return response.json()
+//                   })
+//                   .then((message2) => {
+//                     message2 = JSON.stringify(message2)
+//                     console.log(message2)//object value
+//                     console.log("5:receiving data api=>extension (background)" )
+
+                  
+  
+//                     return document.getElementById('result').innerHTML = message2
+                    
+
+                    
+//                   })
+
+
+                 
+           
+                  
+//                 }})
+
+//             //実行結果をポップアップウィンドウへ表示
+           
+//           }
+//         )}
+        
+          
+
+//     function parseDOM()
+// {   
+//     console.log("1")
+//     return document.body.innerHTML;
+   
+    
+
+//     // chrome.runtime.sendMessage(textString, function(sendResponse){
+//     //     console.log(sendResponse)
+//     //     console.log("6:receiving data background=>popup")
+//     //     // sendResponse = JSON.stringify(sendResponse)
+//     //     // sendResponse = JSON.parse(sendResponse)
+    
+        
+//     // })
+
+// }   
+
+// function transform(final)
+
+// {
+
+// }
+    
+
+// /*
+
+// main();
+// async function main()
+// {
+// 	//アクティブなタブを取得
+// 	let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+// 	//アクティブなタブでJavaScript(parseDOM)を実行
+// 	chrome.scripting.executeScript({
+// 		target:{tabId:tab.id},
+// 		func:parseDOM
+// 	}).then(function (r) {
+// 		//実行結果をポップアップウィンドウへ表示
+//         console.log(r)
+//         console.log(2)
+// 		document.getElementById('result').innerHTML = r[0].result;
+// 	});
+// }
+// function parseDOM()
+// {
+//     console.log('1')
+// 	return document.body.innerHTML;
+// }
+
+// */
--- popup.js
+++ popup.js
@@ -1,73 +1,141 @@
 
-// main();
-// async function main()
-// {
-// 	//アクティブなタブを取得
-// 	let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-// 	//アクティブなタブでJavaScript(parseDOM)を実行
-// 	chrome.scripting.executeScript({
-// 		target:{tabId:tab.id},
-// 		func:parseDOM
-// 	}).then(function (final) {
-// 		//実行結果をポップアップウィンドウへ表示
-// 		document.getElementById('result').outerHTML = final[0].result;
-// 	}).catch(function (error) {
-//         message.innerText = 'There was an error injecting script : \n' + error.message;
-//     });
-// }
-// function parseDOM()
-// {
-//     text = document.body.innerHTML;
-//     textString = text.toString()
-//     chrome.tabs.sendMessage(tabs[0].id, textString, function(){
-//        return final = Response
-        
-//         })
+main()
 
+
+async function main()
+{
+ 
+      
+      
+	//アクティブなタブを取得
+	let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+	//アクティブなタブでJavaScript(parseDOM)を実行
+	await chrome.scripting.executeScript({
+		target:{tabId:tab.id, allFrames: true},
+		func:parseDOM
+    }).then(injectionResults => {
+      injectionResults = injectionResults[0].result
+
+      data = injectionResults.toString()
+      console.log(data)
+      console.log("2:getting html success")
+    })
+           
+  var newHomonymArray = []
+
+  await fetch('http://localhost:3000/api/blinji/gettingListReq')                
+  .then((response) => {
+    return  response.json()
+  })
+  .then((message1) => {
+    message1 = JSON.stringify(message1)
+    console.log(message1)
+    console.log("3:receiving data api=>extension " )
+    //hardwordリスト（文字列）を整える["hard","暑い","氏"]
+    var message1 = message1.replace(/[\"]/g,"") //""を消す
+    var message1 = message1.slice( 1 ) ;
+    var message1 = message1.slice( 0, -1 ) ;
+
+    console.log(message1)//hard,暑い,氏
+
+    var homonymArray = []
+    homonymArray = message1.split(",");
+    console.log(homonymArray)
+
+
+    // リストと一致するhardwordを調べる 
+    
+    for( var i =0; i< homonymArray.length; ++i ) {
+      if( data.match(homonymArray[i])){
+        var result = data.match(homonymArray[i]) 
+        console.log(result[0]) 
+        newHomonymArray.push(result[0])
+      }
+    }
+                  
+    console.log(newHomonymArray)
+  })
+
+    //apiへ送り、easywordsを得る
+  for(var i=0; i<newHomonymArray.length; ++i){ 
+  await fetch('http://localhost:3000/api/blinji/easyWordReq/' + newHomonymArray[i],{
+    method:"POST",
+    body: newHomonymArray[i]
+   })
+  .then((response) => {              
+    return response.json()
+   })
+  .then((message2) => {
+    message2 = JSON.stringify(message2)
+    console.log(message2)//object value
+    console.log(newHomonymArray[i])
+    console.log("5:receiving data api=>extension (background)" )
+      let reg = newHomonymArray[i]
+      let rep = message2
+    data = data.replace(new RegExp(reg,'g'),new RegExp(rep))
+  })
+    final = data
+    
+                    
+
+                    
    
-//   //  var final = textString.replace(/大/g,'jkkkkkkk')
-//  console.log(final)
-//    return final
-// }
 
 
+  }
+  console.log(final)
+  document.getElementById('result').innerHTML = final
+}
+        
+          
 
+    function parseDOM()
+{   
+    console.log("1")
+    return document.body.innerHTML;
+   
+    
 
+    // chrome.runtime.sendMessage(textString, function(sendResponse){
+    //     console.log(sendResponse)
+    //     console.log("6:receiving data background=>popup")
+    //     // sendResponse = JSON.stringify(sendResponse)
+    //     // sendResponse = JSON.parse(sendResponse)
+    
+        
+    // })
 
+}   
 
+function transform(final)
 
+{
 
-
+}
+    
 
 /*
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){  //waiting and getting values, and executing function and sending response
-    if (request !== 'getTab') return   
-    alert('background => popup: message passing ok')
-    sendResponse('received')
 
-    // chrome.scripting.executeScript({
-	//	target:{tabId:tab.id},
-	//	func:parseDOM
-	//})
-
-
-
-
-})
-
--/*
-
+main();
+async function main()
+{
+	//アクティブなタブを取得
+	let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+	//アクティブなタブでJavaScript(parseDOM)を実行
+	chrome.scripting.executeScript({
+		target:{tabId:tab.id},
+		func:parseDOM
+	}).then(function (r) {
+		//実行結果をポップアップウィンドウへ表示
+        console.log(r)
+        console.log(2)
+		document.getElementById('result').innerHTML = r[0].result;
+	});
+}
 function parseDOM()
 {
-    text = document.body.innerHTML;
-
-    textString = text.toString()
-    //return final textString ="kkkkk"
-
-   let final = textString.replace(/大/g,'jkkkkkkk')
-   console.log(final)
-   final = document.getElementById('replacedText').innerHTML
-	
+    console.log('1')
+	return document.body.innerHTML;
 }
 
 */
