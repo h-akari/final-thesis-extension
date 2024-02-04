25e2c8efad864941e941de49c149beca2961f531
h-akari <hieda.akari0930@gmail.com>
2024-02-04 15:28:02 +0900

    コメント追加

--- popup.js
+++ popup.js
@@ -12,18 +12,18 @@ async function main()
 	//アクティブなタブでJavaScript(parseDOM)を実行
 	await chrome.scripting.executeScript({
 		target:{tabId:tab.id, allFrames: true},
-		func:parseDOM
+		func:parseDOM    //htmlを取得する関数をブラウザ上で実行
     }).then(injectionResults => {
-      injectionResults = injectionResults[0].result
+      injectionResults = injectionResults[0].result  //HTML取得
 
-      data = injectionResults.toString()
+      data = injectionResults.toString()   //取得したhtmlを変数dataに格納
       console.log(data)
       console.log("2:getting html success")
-    })
+    }) 
            
   var newHomonymArray = []
 
-  await fetch('http://localhost:3000/api/blinji/gettingListReq')                
+  await fetch('http://localhost:3000/api/blinji/gettingListReq')     //databaseからすべての難語を取得           
   .then((response) => {
     return  response.json()
   })
@@ -31,7 +31,7 @@ async function main()
     message1 = JSON.stringify(message1)
     console.log(message1)
     console.log("3:receiving data api=>extension " )
-    //hardwordリスト（文字列）を整える["hard","暑い","氏"]
+    //hardwordリスト（文字列）を整える（["hard","暑い","氏"]みたいなかたちだから）
     var message1 = message1.replace(/[\"]/g,"") //""を消す
     var message1 = message1.slice( 1 ) ;
     var message1 = message1.slice( 0, -1 ) ;
@@ -40,16 +40,16 @@ async function main()
 
     var homonymArray = []
     homonymArray = message1.split(",");
-    console.log(homonymArray)
+    console.log(homonymArray)  //整形完了。もっと簡略化できると思う笑
 
 
-    // リストと一致するhardwordを調べる 
+    // リスト（すべての難語）と一致するhardword（html上の難語）を調べる 
     
-    for( var i =0; i< homonymArray.length; ++i ) {
-      if( data.match(homonymArray[i])){
-        var result = data.match(homonymArray[i]) 
+    for( var i =0; i< homonymArray.length; ++i ) {  //難語リスト内の単語の数だけループ
+      if( data.match(homonymArray[i])){            
+        var result = data.match(homonymArray[i]) //比較
         console.log(result[0]) 
-        newHomonymArray.push(result[0])
+        newHomonymArray.push(result[0]) //一致した難語を格納
       }
     }
                   
@@ -58,33 +58,33 @@ async function main()
 
   var final
 
-    //apiへ送り、easywordsを得る
+    //抽出した難語をapiへ送り、easywords（平易語）を得る
   for(var i=0; i<newHomonymArray.length; ++i){ 
   await fetch('http://localhost:3000/api/blinji/easyWordReq/' + newHomonymArray[i],{
     method:"POST",
     body: newHomonymArray[i]
    })
-  .then((response) => {              
+  .then((response) => {        //結果の取得        
     return response.json()
    })
-  .then((message2) => {
-    message2 = message2.easyword
+  .then((message2) => {  
+    message2 = message2.easyword    //難語と平易語のセットを変数に格納
     console.log(newHomonymArray[i])
     console.log(message2)//object value
     
-    console.log("5:receiving data api=>extension (background)" )
+    console.log("5:receiving data api=>extension success")
       let reg = newHomonymArray[i]
-      data = data.replace(new RegExp(reg,'g'),message2)
+      data = data.replace(new RegExp(reg,'g'),message2) //HTML上の難語を平易語に変換
   })
     final = data
     
   }
 
   console.log(final)
-  document.getElementById('result').outerHTML = "変換完了"
+  document.getElementById('result').outerHTML = "変換完了" //ポップアップに[変換完了]と出力
 
 
-  let [tab1] = await chrome.tabs.query({ active: true, currentWindow: true });
+  let [tab1] = await chrome.tabs.query({ active: true, currentWindow: true }); //変換し終わったhtmlをブラウザに表示する
   await chrome.scripting.executeScript({
 		target:{tabId:tab1.id, allFrames: true},
 		func:modification,
@@ -98,20 +98,8 @@ async function main()
 {   
     console.log("1")
     return document.body.innerHTML;
-  
-   
-    
-
-    // chrome.runtime.sendMessage(textString, function(sendResponse){
-    //     console.log(sendResponse)
-    //     console.log("6:receiving data background=>popup")
-    //     // sendResponse = JSON.stringify(sendResponse)
-    //     // sendResponse = JSON.parse(sendResponse)
-    
-        
-    // })
-
 }   
+
 
 function modification(final)
 {   
@@ -121,29 +109,12 @@ function modification(final)
 
 }
     
-
-/*
-
-main();
-async function main()
-{
-	//アクティブなタブを取得
-	let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-	//アクティブなタブでJavaScript(parseDOM)を実行
-	chrome.scripting.executeScript({
-		target:{tabId:tab.id},
-		func:parseDOM
-	}).then(function (r) {
-		//実行結果をポップアップウィンドウへ表示
-        console.log(r)
-        console.log(2)
-		document.getElementById('result').innerHTML = r[0].result;
-	});
-}
-function parseDOM()
-{
-    console.log('1')
-	return document.body.innerHTML;
-}
-
-*/
+//もしbackgraundを使う場合はこんな感じでデータの受け渡しするよ
+    // chrome.runtime.sendMessage(textString, function(sendResponse){
+    //     console.log(sendResponse)
+    //     console.log("6:receiving data background=>popup")
+    //     // sendResponse = JSON.stringify(sendResponse)
+    //     // sendResponse = JSON.parse(sendResponse)
+    
+        
+    // })
